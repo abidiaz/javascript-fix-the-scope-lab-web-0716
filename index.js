603e1d012622ed076bd361c70cac26e25850c16a
Abigail Diaz <github email address>
2016-08-31 19:53:59 -0400

    Completed JS Fix the Scope lab

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// funkyFunction() - returns a function
+// theFunk - is "FUNKY!"
+
 var animal = 'dog'
 
 function myAnimal() {
@@ -5,17 +8,13 @@ function myAnimal() {
 }
 
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
+  var animal = 'cat'
   return animal
 }
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
   const two = 2
+  return n + two  
 }
 
 var funkyFunction = function() {
@@ -25,4 +24,4 @@ var funkyFunction = function() {
 }
 
 // We want this to return a function that returns "FUNKY!" -- how can we accomplish that?
-var theFunk = funkyFunction
+var theFunk = funkyFunction()();
